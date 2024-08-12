2004e6d1e092ea8cb9af73363ac84790a1efa5f1
Priya Rathor <rathorpriya1718@gmail.com>
2024-08-12 19:36:29 +0530

    commit2

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import HeroSection from './component/Hero';
+import Navigation from './component/Navigation'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Navigation/>
+      <HeroSection />
+    </div>
+  )
+}
+
+export default App;
